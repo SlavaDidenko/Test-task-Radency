69ab650fdbb3639a42d764720a2d6585cac99626
Viacheslav <105516738+SlavaDidenko@users.noreply.github.com>
2022-09-18 12:16:55 +0300

    Update test-task.js

--- test-task.js
+++ test-task.js
@@ -8,15 +8,34 @@ function loadGrain(levels) {
   let finish = levels.length;
 
   for (let k = levels.length - 1; k > 1; k--) {
+
     if (levels[k] < levels[k - 1]) {
+
+      if (levels[k - 1] > levels[k - 2]) {
+        finish -= 1;
+        
+        if (levels[levels.length - 1] <= levels[k - 1]) {
+          result = resultSegment;
+          break;
+        } 
+        result = resultSegment - (levels[levels.length - 1] - levels[k - 1]) * width;
+        break;
+      }
+
       finish -= 1;
       continue;
     }
-    break;
-  }
-  
-  for (let i = 1; i < finish ; i++) {
 
+    finish -= 1;
+    resultSegment += levels[levels.length - 1] - levels[k - 1];
+    width++;
+  }
+
+  resultSegment = 0;
+  width = 0;
+
+
+  for (let i = 1; i < finish ; i++) {
     if (i == finish - 1) {
       if (levels[higher] <= levels[i]) {
         result += resultSegment;
